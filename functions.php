5aa645750c2fa84fbc81beb7a50a3a84b9edd97c
Chris Rouse <chris@chrisrouse.us>
2013-12-07 21:03:11 -0600

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,7 +1,9 @@
 // Add the following code to the end of functions.php in your Genesis child theme.
 
+
+/** Load FitVids
 add_action('genesis_after_footer', 'child_script_managment');
 function child_script_managment() {
-      wp_enqueue_script( 'fitvids', get_stylesheet_directory_uri() . '/js/FitVids.js-master/jquery.fitvids.js', array('jquery'), '', TRUE);
+      wp_enqueue_script( 'fitvids', get_stylesheet_directory_uri() . '/js/jquery.fitvids.js', array('jquery'), '', TRUE);
       wp_enqueue_script( 'fitvids-xtra', get_stylesheet_directory_uri() . '/js/FitVids.js', array(), '', TRUE);
 }
